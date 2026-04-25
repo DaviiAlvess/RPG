e116abb98191e5bcb9668658f20e972e5ca0de01
davialvs908 <alunodavi14@gmail.com>
2026-04-24 22:22:44 -0300

    nossa atualizaçoes 06

--- rpg-yronwood/next.config.js
+++ rpg-yronwood/next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Isso avisa a Vercel para NÃO barrar o site por causa de variáveis não usadas
+    ignoreDuringBuilds: true,
+  },
+};
+
 module.exports = nextConfig;
